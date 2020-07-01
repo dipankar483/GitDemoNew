566977eb95ffff940c05052493c6cf1fed4930ce
dipankar <dipankar483@gmail.com>
2020-07-01 23:15:02 +0530

    US guy commits Keep moving forward

--- src/main/java/qaclickacademy/App.java
+++ src/main/java/qaclickacademy/App.java
@@ -11,9 +11,19 @@ public class App
         System.out.println( "Hello World!" );
     }
     
+    //fff
+    //hghghg
     public static void test() {
     	
     	System.out.println("Try1");
-    	System.out.println("Try1");
+    	System.out.println("Try2");
+    }
+    
+    //fgfg
+    //vhvhv
+ public static void test1() {
+    	
+    	System.out.println("Try3");
+    	System.out.println("Try4");
     }
 }
---
+++ src/main/java/qaclickacademy/NewCode.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+public class NewCode {
+	
+	//fff
+    //hghghg
+    public static void test() {
+    	
+    	System.out.println("NewTry1");
+    	System.out.println("NewTry2");
+    }
+
+}
